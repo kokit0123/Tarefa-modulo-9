8b2a92952b8b5a20176f195c2ded4b8547e41750
Lucas Godoy <lucas_godoy20@hotmail.com>
2023-09-18 12:34:31 -0400

    Tarafera modulo 9

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('#taskForm').submit(function(event) {
+        event.preventDefault();
+        var taskName = $('#newTask').val();
+        if (taskName) {
+            $('#taskList').append('<li>' + taskName + '</li>');
+            $('#newTask').val('');
+        }
+    });
+
+    $('#taskList').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
+
+
+
